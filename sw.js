6c0eec85f6d42efd283025254df48d23812ab673
maiconautomacaosc-cell <maiconautomacaosc@gmail.com>
2026-09-18 09:19:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'juliane-boone-v0.1.26';
+const CACHE = 'juliane-boone-v0.1.27';
 const CORE = ['./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/store.js','./js/data.js','./js/utils.js'];
 
 self.addEventListener('install', event => {
